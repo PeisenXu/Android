40277f5840af7d6be4716969886d554bf919bcce
Sena <admin@xupeisen.com>
2018-08-22 15:57:08 +0800

    新增首页悬浮按钮

--- LCGSen/app/src/main/java/com/lcgsen/doview/FragmentUtils.java
+++ LCGSen/app/src/main/java/com/lcgsen/doview/FragmentUtils.java
@@ -2,20 +2,20 @@ package com.lcgsen.doview;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.lcgsen.master.R;
 
 public class FragmentUtils extends Fragment {
 
     private TextView tv;
-    private WebView webView;
+    private FloatingActionButton floatingView;
 
     public static FragmentUtils newInstance(String name) {
 
@@ -45,31 +45,20 @@ public class FragmentUtils extends Fragment {
     }
 
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(final View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         Bundle bundle = getArguments();
         Object nameObj = bundle.get("name");
         if (bundle != null && nameObj != null) {
             if ("首页".equalsIgnoreCase(nameObj.toString())) {
-                webView = (WebView) view.findViewById(R.id.webview);
-                webView.loadUrl("http://www.baidu.com");
-                WebSettings webSettings = webView.getSettings();
-                webSettings.setJavaScriptEnabled(true);//允许使用js
+                floatingView = (FloatingActionButton) view.findViewById(R.id.fab);
+                floatingView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(view.getContext(),"FAB clicked",Toast.LENGTH_SHORT).show();
+                    }
+                });
 
-                /**
-                 * LOAD_CACHE_ONLY: 不使用网络，只读取本地缓存数据
-                 * LOAD_DEFAULT: （默认）根据cache-control决定是否从网络上取数据。
-                 * LOAD_NO_CACHE: 不使用缓存，只从网络获取数据.
-                 * LOAD_CACHE_ELSE_NETWORK，只要本地有，无论是否过期，或者no-cache，都使用缓存中的数据。
-                 */
-                webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);//不使用缓存，只从网络获取数据.
-
-                // 支持屏幕缩放
-                webSettings.setSupportZoom(true);
-                webSettings.setBuiltInZoomControls(true);
-
-                // 不显示webview缩放按钮
-                // webSettings.setDisplayZoomControls(false);
             } else {
                 tv = (TextView) view.findViewById(R.id.fragment_test_tv);
                 String name = nameObj.toString();
