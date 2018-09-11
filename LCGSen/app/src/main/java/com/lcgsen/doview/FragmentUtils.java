e38ff24e6baa0862a18ac28ac85438d94fdeff28
Sena <admin@xupeisen.com>
2018-09-11 15:26:38 +0800

    首页修改为ListView

--- LCGSen/app/src/main/java/com/lcgsen/doview/FragmentUtils.java
+++ LCGSen/app/src/main/java/com/lcgsen/doview/FragmentUtils.java
@@ -1,5 +1,7 @@
 package com.lcgsen.doview;
 
+import android.content.ClipData;
+import android.database.DataSetObserver;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -7,20 +9,30 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.lcgsen.master.MainActivity;
 import com.lcgsen.master.R;
 import com.lcgsen.utils.DragFloatActionButton;
 import com.lcgsen.utils.ViewHelper;
 import com.lcgsen.utils.custom.EllipsizingTextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FragmentUtils extends Fragment {
 
     private TextView tv;
     private DragFloatActionButton floatingView;
+    private ListView lv;
     private LinearLayout linearLayout;
+
 
     public static FragmentUtils newInstance(String name) {
 
@@ -64,20 +76,7 @@ public class FragmentUtils extends Fragment {
                     }
                 });
 
-                addTextView(view, 0, 0, Color.argb(60, 200, 200, 200));
-                addTextView(view, 0, 0, Color.argb(70, 200, 200, 200));
-                addTextView(view, 0, 0, Color.argb(80, 200, 200, 200));
-                addTextView(view, 0, 0, Color.argb(90, 200, 200, 200));
-                addTextView(view, 0, 0, Color.argb(100, 200, 200, 200));
-                addTextView(view, 0, 0, Color.argb(110, 200, 200, 200));
-                addTextView(view, 0, 0, Color.argb(120, 200, 200, 200));
-                addTextView(view, 0, 0, Color.argb(100, 200, 200, 200));
-                addTextView(view, 0, 0, Color.argb(110, 200, 200, 200));
-                addTextView(view, 0, 0, Color.argb(120, 200, 200, 200));
-                addTextView(view, 0, 0, Color.argb(130, 200, 200, 200));
-                addTextView(view, 0, 0, Color.argb(140, 200, 200, 200));
-                addTextView(view, 0, 0, Color.argb(150, 200, 200, 200));
-                addTextView(view, 0, 0, Color.argb(160, 200, 200, 200));
+                addTextView(view);
 
             } else {
                 tv = (TextView) view.findViewById(R.id.fragment_test_tv);
@@ -87,38 +86,28 @@ public class FragmentUtils extends Fragment {
         }
     }
 
-    private LinearLayout addTextView(final View view, int height, int width, int color) {
+    private ListView addTextView(final View view) {
+        lv = view.findViewById(R.id.lv); // 初始化控件
+        // 实例化 adapter
+        String[] data = new String[]{"暹罗猫", "布偶猫", "折耳猫", "短毛猫", "波斯猫", "蓝猫", "森林猫",
+                "孟买猫", "缅因猫", "埃及猫", "伯曼猫", "缅甸猫", "新加坡猫", "美国短尾猫", "巴厘猫"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(view.getContext(), android.R.layout.simple_list_item_1, data);
+        lv.setAdapter(adapter);
 
-        LinearLayout linearLayout = (LinearLayout) view.findViewById(R.id.home_line);
-
-        EllipsizingTextView textView = new EllipsizingTextView(view.getContext());
-        textView.setMaxLines(5);
-        textView.setText("\t海军蓝\t#000080\t0,0,128\n" +
-                "　\tRoyalBlue\t皇家蓝\t#4169E1\t65,105,225\n" +
-                "　\tCornflowerBlue\t矢车菊的蓝色\t#6495ED\t100,149,237\n" +
-                "　\tLightSteelBlue\t淡钢蓝\t#B0C4DE\t176,196,222\n" +
-                "　\tLightSlateGray\t浅石板灰\t#778899\t119,136,153\n" +
-                "　\tSlateGray\t石板灰\t#708090\t112,128,144\n" +
-                "　\tDoderBlue\t道奇蓝\t#1E90FF\t30,144,255\n" +
-                "　\tAliceBlue\t爱丽丝蓝\t#F0F8FF\t240,248,255\n" +
-                "　\tSteelBlue\t钢蓝\t#4682B4\t70,130,180\n" +
-                "　\tLightSkyBlue\t淡蓝色\t#87CEFA\t135,206,250\n" +
-                "　\tSkyBlue\t天蓝色\t#87CEEB\t135,206,235\n" +
-                "　\tDeepSkyBlue\t深天蓝\t#00BFFF\t0,191,255\n" +
-                "　\tLightBLue\t淡蓝\t#ADD8E6\t173,216,230\n" +
-                "　\tPowDerBlue\t火药蓝\t#B0E0E6");
-        // textView.setBackgroundColor(color);
-        textView.setPadding(10, 10, 10, 10);
-        // recyclerView.addView(view.findViewById(R.id.fragment_test_tv));
-        linearLayout.addView(textView);
-
-        ViewHelper.setMargins(textView, 0, 0, 0, ViewHelper.getScreenHeight(view.getContext()) / 9 / 3);
-
-        //设置recyclerView高度
-        ViewGroup.LayoutParams layoutParams = textView.getLayoutParams();
-        textView.setLayoutParams(layoutParams);
-
-        return linearLayout;
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long id) {
+                /*
+                 * 点击列表项时触发onItemClick方法，四个参数含义分别为
+                 * arg0：发生单击事件的AdapterView
+                 * arg1：AdapterView中被点击的View
+                 * position：当前点击的行在adapter的下标
+                 * id：当前点击的行的id
+                 */
+                Toast.makeText(view.getContext(), "您选择的是" + lv.getAdapter().getItem(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+        return lv;
     }
 
 
