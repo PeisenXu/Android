5dc8f4b42629cd5550102642340867a6d3450019
PeisenXu <admin@xupeisen.com>
2018-10-12 19:01:12 +0800

    界面优化

--- LCGSen/app/src/main/java/com/lcgsen/master/AboutActivity.java
+++ LCGSen/app/src/main/java/com/lcgsen/master/AboutActivity.java
@@ -1,5 +1,7 @@
 package com.lcgsen.master;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -15,7 +17,7 @@ public class AboutActivity extends AppCompatActivity {
                 .isRTL(false)
                 .setImage(R.drawable.favicon)// 图片
                 .setDescription("不怕路远，就怕志短。\n（应用市场请在酷安打开）")// 介绍
-                .addItem(new Element().setTitle("Version 1.4"))
+                .addItem(new Element().setTitle("Version " + getVerName(AboutActivity.this)))
                 .addGroup("与我联系")
                 .addEmail("admin@xupeisen.com")//邮箱
                 .addWebsite("http://www.xupeisen.com")// 网站
@@ -23,5 +25,21 @@ public class AboutActivity extends AppCompatActivity {
                 .addGitHub("PeisenXu")// github
                 .create();
         setContentView(aboutPage);
+    }
+
+    /**
+     * 获取版本号名称
+     *
+     * @param context 上下文
+     * @return
+     */
+    public static String getVerName(Context context) {
+        String verName = "";
+        try {
+            verName = context.getPackageManager().getPackageInfo(context.getPackageName(), 0).versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return verName;
     }
 }
