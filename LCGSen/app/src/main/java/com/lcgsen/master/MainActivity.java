aed79defd75ef7f8f097fe4cab47d3e6b33a6b89
PeisenXu <admin@xupeisen.com>
2018-12-27 07:32:59 +0800

    首页重画

--- LCGSen/app/src/main/java/com/lcgsen/master/MainActivity.java
+++ LCGSen/app/src/main/java/com/lcgsen/master/MainActivity.java
@@ -5,43 +5,42 @@ import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.DividerItemDecoration;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
-import android.text.ClipboardManager;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.a520wcf.yllistview.YLListView;
+import com.allen.library.SuperTextView;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.lcgsen.entity.AccountTask;
 import com.lcgsen.enums.DBServiceError;
-import com.lcgsen.master.adapter.MyRecyclerViewAdapter;
 import com.lcgsen.utils.HttpUtils;
 import com.lcgsen.utils.SharedUtils;
 import com.lcgsen.utils.ViewHelper;
 import com.lcgsen.utils.httpurlconnectionutil.HttpCallbackStringListener;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -51,7 +50,7 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
     private View headerView; // 左滑动菜单 --> 侧滑顶部布局
     private ImageView home_left_img; // 首页左上角菜单图标
     private LinearLayout home_layout; // 首页主布局
-    private CoordinatorLayout home_recycler_view; // 首页嵌入布局
+    private RelativeLayout homeListView; // 首页动态嵌入布局
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,15 +67,15 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
         home_left_img = findViewById(R.id.main_menu);
         home_layout = findViewById(R.id.home_layout);
 
-        // 加载数据 首页逻辑于 init()
+        // 加载数据
         initWindow();
-        init2();
+        initHomeView();
 
         // 设置侧滑栏动态信息
         userName.setText(SharedUtils.getParam(MainActivity.this, "USER_NAME", "未知登陆").toString());
         userCreateTime.setText(SharedUtils.getParam(MainActivity.this, "USER_CREATE_TIME", "加入时间:未来").toString());
 
-        navigationView.getMenu().add(1,1,2,"语音智能");//需要获取id的话，id就等于1；
+        navigationView.getMenu().add(1, 1, 2, "语音智能");//需要获取id的话，id就等于1；
 /*        navigationView.getMenu().add(1,2,2,"图库");
         navigationView.getMenu().add(1,3,2,"上传");*/
 
@@ -154,24 +153,160 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
      * 初始化数据
      */
 
+    class DemoAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return position;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // https://blog.csdn.net/u011622280/article/details/77159837
+
+            SuperTextView s = (SuperTextView) getLayoutInflater().inflate(R.layout.home_supertext_view, null);
+            s.setLeftTopString("")
+                    .setLeftString("")
+                    .setLeftBottomString("")
+                    .setCenterTopString("")
+                    .setCenterString(list.get(position))
+                    .setCenterBottomString("")
+                    .setRightTopString("")
+                    .setRightString("")
+                    .setRightBottomString("")
+                    .setLeftIcon(0)
+                    .setRightIcon(0)
+                    .setCbChecked(true)
+                    .setCbBackground(null)
+                    .setLeftTvDrawableLeft(null)
+                    .setLeftTvDrawableRight(null)
+                    .setCenterTvDrawableLeft(null)
+                    .setCenterTvDrawableRight(null)
+                    .setRightTvDrawableLeft(null)
+                    .setRightTvDrawableRight(null);
+
+            s.setShapeCornersRadius(20)
+                    .setShapeCornersTopLeftRadius(40)
+                    .setShapeCornersBottomLeftRadius(20)
+                    .setShapeCornersTopRightRadius(20)
+                    .setShapeCornersBottomRightRadius(20)
+                    .setShapeStrokeColor(getResources().getColor(R.color.colorPrimary))
+                    .setShapeStrokeWidth(1)
+                    .setShapeSrokeDashWidth(1)
+                    .setShapeStrokeDashGap(5)
+                    .setShapeSolidColor(getResources().getColor(R.color.white))
+                    .setShapeSelectorNormalColor(getResources().getColor(R.color.white))
+                    .setShapeSelectorPressedColor(getResources().getColor(R.color.common_pressed))
+                    .useShape();//设置完各个参数之后这句调用才生效
+            return s;
+        }
+
+    }
+
     private List<String> list;
-    private RecyclerView mRecyclerView;
 
-    private void init2() {
-        home_recycler_view = (CoordinatorLayout) getLayoutInflater().inflate(R.layout.recycler_view, null);
-        home_layout.addView(home_recycler_view);
 
-        mRecyclerView = findViewById(R.id.recycler_view);
+    private Drawable loadImageFromNetwork(String imageUrl) {
 
-        addView();
+        Drawable drawable = null;
+        try {
+            // 可以在这里通过文件名来判断，是否本地有此图片
+            drawable = Drawable.createFromStream(new URL(imageUrl).openStream(), "image.jpg");
+        } catch (IOException e) {
+            Log.d("test", e.getMessage());
+        }
+        if (drawable == null) {
+            Log.d("test", "null drawable");
+        } else {
+            Log.d("test", "not null drawable");
+        }
 
-        //设置添加或删除item时的动画，这里使用默认动画
-        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
-        //设置RecyclerView管理器
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this, LinearLayoutManager.VERTICAL, false));
-        mRecyclerView.addItemDecoration(new DividerItemDecoration(MainActivity.this, LinearLayoutManager.VERTICAL));
+        return drawable;
+    }
 
-        setGestureListener();
+    private void initHomeView() {
+        homeListView = (RelativeLayout) getLayoutInflater().inflate(R.layout.recycler_view, null);
+        home_layout.addView(homeListView);
+
+        final YLListView listView = (YLListView) findViewById(R.id.listView);
+        // 不添加也有默认的头和底
+        final View topView = View.inflate(this, R.layout.home_top, null);
+
+        // 设置首页列表下拉图片---发http请求是不能在主线程里面发
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                final Drawable drawable = loadImageFromNetwork("https://www.xupeisen.com/app/android/img/home_top.png");
+                // post() 特别关键，就是到UI主线程去更新图片
+                topView.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                            topView.setBackground(drawable);
+                        }
+                    }
+                });
+            }
+        }).start();
+
+
+        listView.addHeaderView(topView);
+        // View bottomView=new View(getApplicationContext());
+        // istView.addFooterView(bottomView);
+
+        // 顶部和底部也可以固定最终的高度 不固定就使用布局本身的高度
+        // listView.setFinalBottomHeight(100);
+        listView.setFinalTopHeight(1);
+
+
+        //返回字符串
+        try {
+            String url = DBServiceError.DB_SERVICE_URL.getMsg() + "/app/driver/driver.select.php?select=SELECT%20*%20FROM%20account_task%20ORDER%20BY%20RAND()%20LIMIT%208";
+            HttpUtils.doGet(MainActivity.this, url, new HttpCallbackStringListener() {
+                @Override
+                public void onFinish(String response) {
+                    Gson gson = new Gson();
+                    List<AccountTask> accountTasks = gson.fromJson(response, new TypeToken<List<AccountTask>>() {
+                    }.getType());
+                    list = new ArrayList<String>();
+                    for (AccountTask accountTask : accountTasks) {
+                        list.add(accountTask.getTitle());
+                    }
+                    Toast.makeText(MainActivity.this, "加载成功", Toast.LENGTH_SHORT).show();
+
+                    // 数据获取成功后， 加载数据
+                    listView.setAdapter(new DemoAdapter());
+
+                    //YLListView默认有头和底  处理点击事件位置注意减去
+                    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            position = position - listView.getHeaderViewsCount();
+                        }
+                    });
+                }
+
+                @Override
+                public void onError(Exception e) {
+                    Log.e("错误", e.toString());
+                    Toast.makeText(MainActivity.this, DBServiceError.DB_SERVICE_ERROR.getMsg(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(MainActivity.this, DBServiceError.DB_SERVICE_ERROR.getMsg(), Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     private void initWindow() {
@@ -226,88 +361,5 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
     @Override
     public void onClick(View v) {
 
-    }
-
-    private float mPosX = 0, mPosY = 0, mCurPosX = 0, mCurPosY = 0;
-
-    private void setGestureListener() {
-        // 是要监听的视图 mAlertImageViewD
-        // TODO
-        mRecyclerView.setOnTouchListener(new View.OnTouchListener() {
-
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                // TODO Auto-generated method stub
-                switch (event.getAction()) {
-
-                    case MotionEvent.ACTION_DOWN:
-                        mPosX = event.getX();
-                        mPosY = event.getY();
-                        break;
-                    case MotionEvent.ACTION_MOVE:
-                        mCurPosX = event.getX();
-                        mCurPosY = event.getY();
-
-                        break;
-                    case MotionEvent.ACTION_UP:
-                        if ((mPosX - mCurPosX) > 40) {
-                            addView();
-                        }
-                        break;
-                }
-                return true;
-            }
-
-        });
-    }
-
-    public void addView() {
-        //返回字符串
-        try {
-            String url = DBServiceError.DB_SERVICE_URL.getMsg() + "/app/driver/driver.select.php?select=SELECT%20*%20FROM%20account_task%20ORDER%20BY%20RAND()%20LIMIT%208";
-            HttpUtils.doGet(MainActivity.this, url, new HttpCallbackStringListener() {
-                @Override
-                public void onFinish(String response) {
-                    Gson gson = new Gson();
-                    List<AccountTask> accountTasks = gson.fromJson(response, new TypeToken<List<AccountTask>>() {
-                    }.getType());
-                    list = new ArrayList<String>();
-                    for (AccountTask accountTask : accountTasks) {
-                        list.add(accountTask.getTitle());
-                    }
-                    Toast.makeText(MainActivity.this, "加载成功", Toast.LENGTH_SHORT).show();
-
-
-                    //初始化适配器
-                    MyRecyclerViewAdapter mAdapter = new MyRecyclerViewAdapter(list);
-                    mAdapter.setOnItemClickListener(new MyRecyclerViewAdapter.OnItemClickListener() {
-                        @Override
-                        public void onItemClick(View view, int position, String data) {
-                        }
-                    });
-                    mAdapter.setOnItemLongClickListener(new MyRecyclerViewAdapter.OnItemLongClickListener() {
-                        @Override
-                        public void onItemLongClick(View view, int position, String data) {
-                            // 兼容旧版Android复制剪贴板
-                            ClipboardManager cm = (ClipboardManager) MainActivity.this.getSystemService(Context.CLIPBOARD_SERVICE);
-                            // 将文本内容放到系统剪贴板里。
-                            cm.setText(data);
-                            Toast.makeText(MainActivity.this, DBServiceError.COPY_SUCCESS.getMsg(), Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                    //设置适配器
-                    mRecyclerView.setAdapter(mAdapter);
-                }
-
-                @Override
-                public void onError(Exception e) {
-                    Log.e("错误", e.toString());
-                    Toast.makeText(MainActivity.this, DBServiceError.DB_SERVICE_ERROR.getMsg(), Toast.LENGTH_SHORT).show();
-                }
-            });
-        } catch (Exception e) {
-            e.printStackTrace();
-            Toast.makeText(MainActivity.this, DBServiceError.DB_SERVICE_ERROR.getMsg(), Toast.LENGTH_SHORT).show();
-        }
     }
 }
